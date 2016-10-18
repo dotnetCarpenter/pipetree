3468ca6404cc5736388b74c61548dc721a1e34a3
dotnetCarpenter <jon.ronnenberg@gmail.com>
2016-10-18 21:10:20 -0200

    travis support and small fixes

--- spec/integration/downloadUrl.js
+++ spec/integration/downloadUrl.js
@@ -3,7 +3,7 @@
 "use strict"
 
 const select = require("../../lib/root").select
-const listen = require("../../lib/root").listen
+const listen = require("../../lib/listen").listen
 
 // get a function to traverse a tree - this one returns the first occurance
 // of the field "browser_download_url"
@@ -14,7 +14,7 @@ listen.start()
 
 // use this function when the piped input is read
 select.use = () => {
-	// use the
+	// use the querySelector from select on the parsed JSON piped input
 	const downloadUrl = getDownloadUrl( listen.collectJSON() )
 	console.log(downloadUrl)
 }
