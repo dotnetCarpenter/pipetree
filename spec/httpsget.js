7ceeef9b3c93c71a6e6a743ffc1815c3950a4da3
dotnetCarpenter <jon.ronnenberg@gmail.com>
2016-10-19 14:56:41 -0200

    npm link before test on continues integration servers

---
+++ spec/httpsget.js
@@ -0,0 +1,7 @@
+"use strict"
+
+const tap 		= require("tap")
+const url			= require("url")
+const listen	= require("../lib/root").listen
+
+
--- spec/listen.js
+++ spec/listen.js
@@ -4,13 +4,11 @@ const tap			= require("tap")
 const listen	= require("../lib/root").listen
 const Listen	= require("../lib/root").Listen
 
-tap.test("listen", t => {
+tap.test("Listen object", t => {
 	t.plan(1)
 
 	let expected = new Listen 
 	let actual = listen
 	t.same(actual, expected, "listen is a shorthand for 'new Listen")
-
-	
 
 })
