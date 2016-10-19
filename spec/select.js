ab3f3914968ceae131398ae130bab6708f00bf73
dotnetCarpenter <jon.ronnenberg@gmail.com>
2016-10-18 23:17:40 -0200

    added a search strategy option to select

--- spec/select.js
+++ spec/select.js
@@ -13,24 +13,25 @@ function parseFile(filename) {
 	return JSON.parse(fs.readFileSync(`${__dirname}/fixtures/${filename}`))
 }
 
-tap.test("select.querySelectorAll", t => {
+tap.test("select.breadthFirst && select.querySelectorAll", t => {
 	t.plan(2)
 
 	const getTags = select.querySelectorAll("tag_name")
-	let expected = [ 'v2.5.2', 'v2.5.1', 'v2.5.0', 'v2.4.1', 'v2.4.0', 'v2.3.4', 'v2.3.3', 'v2.3.2', 'v2.3.1', 'v2.3.0', 'v2.2.1', 'v2.2.0', 'v2.1.3', 'v2.1.2', 'v2.1.1', 'v2.1.0', 'v2.0.2', 'v2.0.1', 'v2.0.0', 'v1.3.1' ]
+	let expected = [ "v2.5.2", "v2.5.1", "v2.5.0", "v2.4.1", "v2.4.0", "v2.3.4", "v2.3.3", "v2.3.2", "v2.3.1", "v2.3.0", "v2.2.1", "v2.2.0", "v2.1.3", "v2.1.2", "v2.1.1", "v2.1.0", "v2.0.2", "v2.0.1", "v2.0.0", "v1.3.1" ]
 	let actual = getTags(releases)
 	t.same(actual, expected, "should collect all occurances of 'tag_name'")
 
-	expected = [ 'v2.5.2' ]
+	expected = [ "v2.5.2" ]
 	actual = getTags(latest)
 	t.same(actual, expected, "should collect all occurances of 'tag_name'")
 
 })
 
-tap.test("select.querySelector", t => {
+tap.test("select.breadthFirst && select.querySelector", t => {
 	t.plan(2)
 
 	const getTag  = select.querySelector("tag_name")
+
 	let expected = "v2.5.2"
 	let actual = getTag(releases)
 	t.same(actual, expected, "should find the first occurance of 'tag_name'")
@@ -38,4 +39,45 @@ tap.test("select.querySelector", t => {
 	expected = "v2.5.2"
 	actual = getTag(latest)
 	t.same(actual, expected, "should find the first occurance of 'tag_name'")
+})
+
+tap.test("select.deepFirst && select.querySelectorAll", t => {
+	t.plan(2)
+
+	select.searchStrategy = select.deepFirst
+	const getTags  = select.querySelectorAll("tag_name")
+
+	let expected = [ "v2.5.2", "v2.5.1", "v2.5.0", "v2.4.1", "v2.4.0", "v2.3.4", "v2.3.3", "v2.3.2", "v2.3.1", "v2.3.0", "v2.2.1", "v2.2.0", "v2.1.3", "v2.1.2", "v2.1.1", "v2.1.0", "v2.0.2", "v2.0.1", "v2.0.0", "v1.3.1" ] 
+	let actual = getTags(releases)
+	t.same(actual, expected, "should find the first occurance of 'tag_name'")
+
+	expected = [ "v2.5.2" ]
+	actual = getTags(latest)
+	t.same(actual, expected, "should find the first occurance of 'tag_name'")
+})
+
+tap.test("select.deepFirst && select.querySelector", t => {
+	t.plan(2)
+
+	const getTag  = select.querySelector("tag_name")
+	select.searchStrategy = select.deepFirst
+
+	let expected = "v2.5.2"
+	let actual = getTag(releases)
+	t.same(actual, expected, "should find the first occurance of 'tag_name'")
+
+	expected = "v2.5.2"
+	actual = getTag(latest)
+	t.same(actual, expected, "should find the first occurance of 'tag_name'")
+})
+
+tap.test("select.searchStrategies", t => {
+	t.plan(1)
+
+	const expected = {
+		breadthFirst: 1,
+		deepFirst: 2
+	}
+	const actual = select.searchStrategies
+	t.same(actual, expected, "should give breadthFirst and deepFirst as strategies")
 })
